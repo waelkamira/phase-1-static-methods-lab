1a1b55cd3878487f132f2fe5ba13f95dca9f72f8
wael kamira <ramondm4@gmail.com>
2023-05-27 00:09:01 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    const nonTitleCaseWords = [
+      'the',
+      'a',
+      'an',
+      'but',
+      'of',
+      'and',
+      'for',
+      'at',
+      'by',
+      'from'
+    ];
+
+    const words = string.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !nonTitleCaseWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
 }
+
